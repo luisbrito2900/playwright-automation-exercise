74aadf134d57779e7e600e51522d8c4f55345d60
Luis Brito <luisbrito@Luiss-MacBook-Pro.local>
2025-09-05 20:13:47 -0400

    removing browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,17 +21,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'] },
-    },
   ],
 });
